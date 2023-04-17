ee69ac6f1e793a6158a9557a995089137b0e8f18
João Gabriel Lyra <jgabriellyra@hotmail.com>
2023-04-17 02:49:16 -0300

    iniciando o projeto & requisito 1 concluído

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if type(target_time) == str or not target_time:
+        return None
+    score = 0
+
+    for tuple in permanence_period:
+        if None in tuple or type(tuple[0]) == str or type(tuple[1]) == str:
+            return None
+        if target_time >= tuple[0] and target_time <= tuple[1]:
+            score += 1
+    return score
