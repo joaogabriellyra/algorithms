45e67b98146e86d79d4e6c12a5e73a8f7da88b57
João Gabriel Lyra <jgabriellyra@hotmail.com>
2023-04-17 18:11:03 -0300

    requisito 4 concluído com ressalvas

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,44 @@
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    first_string = merge_sort_word(list(first_string.lower()))
+    second_string = merge_sort_word(list(second_string.lower()))
+
+    anagram = first_string == second_string and (
+        first_string != "" and second_string != "")
+    return (first_string, second_string, anagram)
+
+
+def merge_sort_word(word, start=0, end=None):
+    if end is None:
+        end = len(word)
+
+    if end - start > 1:
+        mid = (start + end) // 2
+        merge_sort_word(word, start, mid)
+        merge_sort_word(word, mid, end)
+        word = merge(word, start, mid, end)
+
+    return "".join(word)
+
+
+def merge(word, start, mid, end):
+    right_side = word[mid:end]
+    left_side = word[start:mid]
+
+    right_index = 0
+    left_index = 0
+
+    for index in range(start, end):
+        if left_index >= len(left_side):
+            word[index] = right_side[right_index]
+            right_index += 1
+        elif right_index >= len(right_side):
+            word[index] = left_side[left_index]
+            left_index += 1
+        elif left_side[left_index] < right_side[right_index]:
+            word[index] = left_side[left_index]
+            left_index += 1
+        else:
+            word[index] = right_side[right_index]
+            right_index += 1
+
+    return word
