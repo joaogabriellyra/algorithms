802892f420ffcb4d99d8f70508b04a4f863fd8b7
João Gabriel Lyra <jgabriellyra@hotmail.com>
2023-04-17 12:28:31 -0300

    requisito 2 concluído

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,13 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError):
+        encrypt_message(1, 2)
+    with pytest.raises(TypeError):
+        encrypt_message("FalleN", "10")
+
+    assert encrypt_message("FalleN", 2) == "Nell_aF"
+    assert encrypt_message("FalleN", 2) != "Ne_llaF"
+    assert encrypt_message("FalleN", 10) == "".join("NellaF")
