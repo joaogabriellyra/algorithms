528c43d375ebbe05d641d107b9b69d9fef20a030
João Gabriel Lyra <jgabriellyra@hotmail.com>
2023-04-17 20:06:34 -0300

    requisito 6 concluído

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,8 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not word:
+        return False
+    reversed = list(word)[-1::-1]
+    for index, letter in enumerate(list(word)):
+        if letter != reversed[index]:
+            return False
+    return True
